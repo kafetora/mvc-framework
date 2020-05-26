f6dc4b9f9798a443b5e5e30712924ec2c2a20a40
kafetora <kafetora@gmail.com>
2020-05-26 21:21:47 +0900

    add

--- core/Controller.php
+++ core/Controller.php
@@ -30,11 +30,24 @@ abstract class Controller{
       $this->forward404();
     }
 
+    if( $this->needsAuthentication( $action ) && !$this->session->isAuthenticated() ){
+      throw new UnauthorizedActionException();
+    }
+
     $content = $this->$action_method( $params );
 
     return $content;
   }
 
+
+  protected function needsAuthentication( $action ){
+
+    if( $this->auth_actions === true || ( is_array( $this->auth_actions ) && in_array( $action, $this->auth_actions ) ) ){
+      return true;
+    }
+
+    return false;
+  }
 
   protected function render( $variables = array(), $template = null, $layout = 'layout' ){
 
@@ -55,6 +68,63 @@ abstract class Controller{
 
     return $view->render( $path, $variables, $layout );
 
+  }
+
+
+  protected function forward404(){
+    throw new HttpNotFoundException( 'Forward 404 page from ' . $this->controller_name . '/' . $this->action_name );
+  }
+
+
+  protected function redirect( $url ){
+
+    if( !preg_match( '#https?://#', $url ) ){
+
+      $protocol = $this->request->isSsl() ? 'https://' : 'http://';
+      $host = $this->request->getHost();
+      $base_url = $this->request->getBaseUrl();
+
+      $url = $protocol . $host . $base_url . $url;
+    }
+    
+
+    $this->response->setStatusCode( 302, 'Found' );
+    $this->response->setHttpHeader( 'Location', $url );
+  }
+
+
+  protected function generateCsrfToken( $form_name ){
+
+    $key = 'scrf_tokens/' . $form_name;
+    $tokens = $this->session->get( $key, array() );
+
+    if( count( $tokens ) >= 10 ){
+      array_shift( $tokens );
+    }
+
+    $token = sha1( $form_name . session_id() . microtime() );
+    $token[] = $token;
+
+    $this->session->set( $key, $tokens );
+
+    return $token;
+
+  }
+
+
+  protected function checkCsrfToken( $form_name, $token ){
+
+    $key = 'csfr_tokens/' . $form_name;
+    $tokens = $this->session->get( $key, array() );
+
+    if( false !== ( $pos = array_search( $token, $tokens, true ) ) ){
+      unset( $tokens[$pos] );
+      $this->session->set( $key, $tokens );
+
+      return true;
+    }
+
+    return false;
   }
 
 }
